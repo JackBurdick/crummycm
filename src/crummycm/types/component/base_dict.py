dd13d7fce725e2096fc443bc4c8aa36b5ea427e6
Jack Burdick <jackbburdick@gmail.com>
2020-10-22 17:36:16 -0700

    base placeholder

--- src/crummycm/types/component/base_dict.py
+++ src/crummycm/types/component/base_dict.py
@@ -1,18 +1,32 @@
 from crummycm.types.base import Base
 
 
-class ValuePlaceholder:
+class Placeholder:
     def __init__(self):
         pass
 
 
-class KeyPlaceholder:
+class ValuePlaceholder(Placeholder):
     def __init__(self):
+        super().__init__()
+        pass
+
+
+class KeyPlaceholder(Placeholder):
+    def __init__(self):
+        super().__init__()
         pass
 
     def __hash__(self):
         # allows to be used as key
         return 0
+
+
+def is_placeholder(cur_obj):
+    try:
+        return issubclass(cur_obj, cl)
+    except TypeError:
+        return False
 
 
 class BaseDict:
@@ -21,6 +35,8 @@ class BaseDict:
             f"in_dict ({type(in_dict)}) of {self.__class__} is expected to be a {dict}, not {type(in_dict)}"
         )
         for k, v in in_dict.items():
+            # TODO: are there any instances in which the k would be hashable but
+            # not a string? an int maybe?
             assert isinstance(k, str) or issubclass(k, KeyPlaceholder), TypeError(
                 f"key {k} is expected to be type {str} or {KeyPlaceholder}, not {type(k)}"
             )
