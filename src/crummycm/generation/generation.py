35f7af1a79e0d10bf9e0cd33a146025fe24924df
Jack Burdick <jackbburdick@gmail.com>
2020-11-03 11:38:18 -0800

    `disallow_unused` in generation

--- src/crummycm/generation/generation.py
+++ src/crummycm/generation/generation.py
@@ -2,7 +2,7 @@ import crummycm as ccm
 from typing import Any
 
 
-def generate(user_in: Any, template: Any):
+def generate(user_in: Any, template: Any, disallow_unused=True):
     raw = ccm.parse(user_in)
-    out = ccm.validate(raw, template)
+    out = ccm.validate(raw, template, disallow_unused)
     return out
